fea044f5ffdd975fe08b589a97ecfb001a56a360
cjskalla <calvin.skalla@gmail.com>
2024-07-04 17:23:13 -0500

    push changes

---
+++ pages/6_🏀_Women's Basketball.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import pandas as pd
+
+st.markdown(
+        """
+        <h3 style="
+            text-align: center;
+            font-family: Forum;
+            font-weight: 400;
+            font-size: 300%;
+            ">
+            Women's Basketball
+        </h1>
+        """,
+        unsafe_allow_html=True
+    )
+
+
+#About
+st.markdown("""
+            <p
+            style="
+                text-align: center;
+                padding-top: 0px;
+                padding-bottom: -100px
+                color: rgb(102, 67, 70);
+            ">
+                Learn about the Women's Basketball through Data !!
+            </p>
+            """,
+            unsafe_allow_html=True
+        )
+
+
+#C 2024
+st.markdown("""
+            <p
+            style="
+                text-align: center;
+                padding-top: 50px;
+                padding-bottom: -100px
+                color: rgb(102, 67, 70);
+            ">
+                <em>© 2024 </em>
+            </p>
+            """,
+            unsafe_allow_html=True
+        )
---
+++ 🏡_Home.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import pandas as pd
+
+st.markdown(
+        """
+        <h3 style="
+            text-align: center;
+            font-family: Forum;
+            font-weight: 400;
+            font-size: 300%;
+            ">
+            Calvin's Dog
+        </h1>
+        """,
+        unsafe_allow_html=True
+    )
+
+
+#About
+st.markdown("""
+            <p
+            style="
+                text-align: center;
+                padding-top: 0px;
+                padding-bottom: -100px
+                color: rgb(102, 67, 70);
+            ">
+                This DOG (data blog) is to demonstrate different interesting concepts about data while we use those interesting concepts to learn about data.    
+            </p>
+            """,
+            unsafe_allow_html=True
+        )
+
+
+#C 2024
+st.markdown("""
+            <p
+            style="
+                text-align: center;
+                padding-top: 50px;
+                padding-bottom: -100px
+                color: rgb(102, 67, 70);
+            ">
+                <em>© 2024 </em>
+            </p>
+            """,
+            unsafe_allow_html=True
+        )
